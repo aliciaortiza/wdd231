b75a4a00d78400d63e96a609431176d6deb68355
aliciaortiza <aliciaortiza@outlook.com>
2026-05-21 19:42:28 -0600

    nav bar and assignment updates

--- scripts/chamber.js
+++ scripts/chamber.js
@@ -6,8 +6,12 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
 
 //hamburguer menu
-document.getElementById('menu-toggle').addEventListener('click', () => {
-    document.querySelector('.header-nav ul').classList.toggle('show');
+const navButton = document.querySelector('#menu-toggle');
+const navBar = document.querySelector('#nav-bar');
+
+navButton.addEventListener('click', () => {
+    navButton.classList.toggle('show');
+    navBar.classList.toggle('show');
 });
 
 ///////
